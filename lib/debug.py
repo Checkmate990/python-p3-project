024d215a204e32881ce23858ad33896fe3241685
Checkmate990 <108345290+Checkmate990@users.noreply.github.com>
2023-03-08 13:38:54 -0700

    Update debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -1,12 +1,13 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
 from db.models import Store, Customer, Part
 
 if __name__ == '__main__':
-    
     engine = create_engine('sqlite:///p3_project.db')
     Session = sessionmaker(bind=engine)
     session = Session()
 
+    # Add code here to query the database or perform other operations
+    # using the SQLAlchemy session object.
+    
     import ipdb; ipdb.set_trace()
