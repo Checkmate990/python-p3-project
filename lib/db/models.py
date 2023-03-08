de9253a9b476ad1aefb00824437e495ae00f5c67
Checkmate990 <108345290+Checkmate990@users.noreply.github.com>
2023-03-08 13:33:34 -0700

    Update models.py

--- lib/db/models.py
+++ lib/db/models.py
@@ -1,62 +1,57 @@
-from sqlalchemy import create_engine
-from sqlalchemy import ForeignKey, Column, DateTime, Integer, String
-from sqlalchemy.orm import relationship, backref
-# from sqlalchemy.orm import sessionmaker
-
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite:///p3_project.db')
+engine = create_engine("sqlite:///p3_project.db")
 
 Base = declarative_base()
 
+
 class Store(Base):
-    __tablename__ = 'stores'
+    """A store that sells parts to customers."""
 
-    id = Column(Integer(), primary_key = True)
-    store_name = Column(String())
-    quantity = Column(Integer())
-    customer_id = Column(Integer(), ForeignKey('customers.id'))
-    parts_id = Column(Integer(), ForeignKey('parts.id'))
+    __tablename__ = "stores"
 
-    customer = relationship('Customer', back_populates='stores')
-    part = relationship('Part', back_populates='stores')
+    id = Column(Integer, primary_key=True)
+    store_name = Column(String)
+    quantity = Column(Integer)
+    customer_id = Column(Integer, ForeignKey("customers.id"))
+    parts_id = Column(Integer, ForeignKey("parts.id"))
 
+    customer = relationship("Customer", back_populates="stores")
+    part = relationship("Part", back_populates="stores")
 
     def __repr__(self):
-        return f"Store {self.id}: " \
-            + f"{self.store_name}, " \
-            + f"{self.quantity}, " 
+        return f"Store {self.id}: {self.store_name}, {self.quantity}"
+
 
 class Customer(Base):
-    __tablename__ = 'customers'
+    """A customer who buys parts from stores."""
 
-    id = Column(Integer(), primary_key = True)
-    name = Column(String())
-    budget = Column(Integer())
-    preference = Column(String())
+    __tablename__ = "customers"
 
-    store = relationship('Store', backref=backref('customer'))
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    budget = Column(Integer)
+    preference = Column(String)
+
+    stores = relationship("Store", back_populates="customer")
 
     def __repr__(self):
-        return f"Customer {self.id}: " \
-            + f"{self.name}, " \
-            + f"Budget: {self.budget}" \
-            + f"Preference: {self.preference}"
+        return f"Customer {self.id}: {self.name}, Budget: {self.budget}, Preference: {self.preference}"
+
 
 class Part(Base):
-    __tablename__ = 'parts'
+    """A part that can be sold by a store to a customer."""
 
-    id = Column(Integer(), primary_key = True)
-    part_name = Column(String())
-    cost = Column(Integer())
-    brand = Column(Integer())
+    __tablename__ = "parts"
 
-    store = relationship('Store', backref=backref('part'))
+    id = Column(Integer, primary_key=True)
+    part_name = Column(String)
+    cost = Column(Integer)
+    brand = Column(Integer)
+
+    stores = relationship("Store", back_populates="part")
 
     def __repr__(self):
-        return f"Part {self.id}: " \
-            + f"{self.part_name}, " \
-            + f"Cost: {self.cost}" \
-            + f"Brand: {self.cost}" \
-    
-    
+        return f"Part {self.id}: {self.part_name}, Cost: {self.cost}, Brand: {self.brand}"
